4ccfd18c8bc7eb80c266c28f66fc928f47fb006a
sulman147 <msulman147@gmail.com>
2020-10-01 00:56:00 +0500

    update .ignore file

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,12 @@
 const User = require("../models/user");
 const { Order } = require("../models/order");
-const { errorHandler } = require("../helpers/dbErrorHandler");
+const { errorHandler } = require("../Helpers/dbErrorHandler");
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        error: "user not found"
+        error: "user not found",
       });
     }
     req.profile = user;
@@ -28,7 +28,7 @@ exports.update = (req, res) => {
     (err, user) => {
       if (err) {
         return res.status(400).json({
-          error: "you are not authorized to perform this action"
+          error: "you are not authorized to perform this action",
         });
       }
       user.hashed_password = undefined;
@@ -41,7 +41,7 @@ exports.update = (req, res) => {
 exports.addOrderToUserHistory = (req, res, next) => {
   let history = [];
 
-  req.body.order.products.forEach(item => {
+  req.body.order.products.forEach((item) => {
     history.push({
       _id: item._id,
       name: item.name,
@@ -49,7 +49,7 @@ exports.addOrderToUserHistory = (req, res, next) => {
       category: item.category,
       quantity: item.count,
       transaction_id: req.body.order.transaction_id,
-      amount: req.body.order.amount
+      amount: req.body.order.amount,
     });
   });
 
@@ -60,7 +60,7 @@ exports.addOrderToUserHistory = (req, res, next) => {
     (error, data) => {
       if (error) {
         return res.status(400).json({
-          error: "Could not update user purchase history"
+          error: "Could not update user purchase history",
         });
       }
       next();
@@ -75,7 +75,7 @@ exports.purchaseHistory = (req, res) => {
     .exec((err, orders) => {
       if (err) {
         return res.status(400).json({
-          error: errorHandler(err)
+          error: errorHandler(err),
         });
       }
       res.json(orders);
